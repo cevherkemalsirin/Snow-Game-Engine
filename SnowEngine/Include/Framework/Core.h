0cdfe1df9784cabc43c071631108f6af32277e81
cevher kemal sirin <76917072+cevherkemalsirin@users.noreply.github.com>
2026-03-28 01:47:23 +0200

    Add Custom Data types for Smart Pointers

--- SnowEngine/Include/Framework/Core.h
+++ SnowEngine/Include/Framework/Core.h
@@ -1,8 +1,18 @@
 #include <print>
+#include <memory>
 
 
 namespace snw
 {
+	template <class T>
+	using unique = std::unique_ptr<T>;
+
+	template <class T>
+	using shared = std::shared_ptr<T>;
+
+	template <class T>
+	using weak = std::weak_ptr<T>;
+
 	template<class ...args>
 	inline void LOG(std::format_string<args...> message, args&&... values)
 	{
